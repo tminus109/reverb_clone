9fc28becd64fee298b8b2c2059da5d75be0018f3
tminus109 <martontibor123@gmail.com>
2021-11-30 15:09:33 +0100

    feat: Banner.tsx, Header.tsx, HomePage.tsx, SignIn.tsx, SignUp.tsx, SignInForm.tsx, SignUpForm.tsx - WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,5 @@
-import React from "react";
 import "./App.css";
 
-// eslint-disable-next-line func-names
-const App = function () {
-  return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
-  );
-};
+const App = () => {};
 
 export default App;
