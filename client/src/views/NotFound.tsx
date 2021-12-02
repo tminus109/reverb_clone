e79a25bb7c60e5139a937b77e323711931f22397
tminus109 <martontibor123@gmail.com>
2021-12-02 18:07:58 +0100

    refact: NotFound.tsx

--- client/src/views/NotFound.tsx
+++ client/src/views/NotFound.tsx
@@ -1,6 +1,14 @@
 // eslint-disable-next-line react/function-component-definition
 function NotFound() {
-  return <h1>Page not found</h1>;
+  return (
+    <div className="notFound">
+      <h1>404</h1>
+      <p>
+        Sorry, you did not break the internet, but we can not find what you are
+        looking for.
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
