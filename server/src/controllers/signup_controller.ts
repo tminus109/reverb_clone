0a9e3ec53bfd4cbd5f47a2a8b68f96bc3ac41c0e
tminus109 <martontibor123@gmail.com>
2021-12-05 19:30:46 +0100

    feat: signup_controller.ts / res, try/catch

--- server/src/controllers/signup_controller.ts
+++ server/src/controllers/signup_controller.ts
@@ -2,10 +2,17 @@ import { Request, Response } from "express";
 import { createNewUserRecord } from "../services/user_service";
 
 export const addNewUser = async (req: Request, res: Response) => {
-  const userId = await createNewUserRecord(
-    req.body.firstName,
-    req.body.lastName,
-    req.body.email,
-    req.body.password
-  );
+  try {
+    await createNewUserRecord(
+      req.body.firstName,
+      req.body.lastName,
+      req.body.email,
+      req.body.password
+    );
+    res.json({ message: "Sign up was successful, you may sign in now" });
+  } catch {
+    (err: Error) => {
+      res.json(err);
+    };
+  }
 };
