bc8238692a90b905a6b6a95058df9ff6c5d2b4d8
tminus109 <martontibor123@gmail.com>
2021-12-05 13:50:44 +0100

    feat: validateSignIn.ts done

--- server/src/middleware/validateSignIn.ts
+++ server/src/middleware/validateSignIn.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from "express";
+import User from "../types/models/User";
+import { getMeAPromise } from "../utils/promiseMe";
 
-const validateSignIn = (req: Request, res: Response, next: NextFunction) => {
+const validateSignIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     if (
       Object.keys(req.body).length === 0 ||
@@ -8,6 +14,20 @@ const validateSignIn = (req: Request, res: Response, next: NextFunction) => {
       !req.body.password
     ) {
       throw new Error("All input fields are required");
+    } else {
+      const query = `SELECT * FROM users where email = ? LIMIT 1`;
+      const user: User = await getMeAPromise(query, [req.body.email]);
+      if (!user || user.password !== req.body.password) {
+        throw new Error("Username or password is incorrect");
+      } else if (user.userStatus === "Pending") {
+        throw new Error(
+          "Account is not verified, click on the verification link sent to your email"
+        );
+      } else {
+        req.body.firstName = user.firstName;
+        req.body.id = user.id;
+        next();
+      }
     }
   } catch {
     (err: Error) => {
@@ -17,7 +37,3 @@ const validateSignIn = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export default validateSignIn;
-
-// user exists?
-// password correct?
-// user status active?
