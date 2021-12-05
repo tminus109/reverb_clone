7047df91c0c9181555075938240cd45b4d9e924e
tminus109 <martontibor123@gmail.com>
2021-12-05 10:14:27 +0100

    feat: isEmailUnique.ts is done

--- server/src/utils/isEmailUnique.ts
+++ server/src/utils/isEmailUnique.ts
@@ -1,5 +1,12 @@
-const isEmailUnique = (email: string): boolean => {
-  return true;
+import { getMeAPromise } from "./promiseMe";
+
+const isEmailUnique = async (email: string): Promise<boolean> => {
+  const query = `SELECT COUNT(*) FROM users WHERE email = ?`;
+  const result = await getMeAPromise(query, [email]);
+  if (result === 0) {
+    return true;
+  }
+  return false;
 };
 
 export default isEmailUnique;
