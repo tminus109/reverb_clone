c3e06ab52ef69bfa10bc5e69279630d3f7445173
tminus109 <martontibor123@gmail.com>
2021-12-13 13:13:08 +0100

    test: SignUpForm.tsx snapshot and render test

---
+++ client/src/__tests__/SignUpForm.test.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import SignUpForm from "../components/forms/SignUpForm";
+
+test("Sign-up form renders without craching", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SignUpForm />, div);
+});
+
+test("Snapshot test", () => {
+  const tree = renderer.create(<SignUpForm />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
