221e4db87636bd217239aa66c54d0dba0c9acc93
tminus109 <martontibor123@gmail.com>
2021-12-13 13:42:19 +0100

    test: App.tsx renders without crashing

--- client/src/__tests__/SignUpForm.test.tsx
+++ client/src/__tests__/SignUpForm.test.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import SignUpForm from "../components/forms/SignUpForm";
 
-test("Sign-up form renders without craching", () => {
+test("Sign-up form renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<SignUpForm />, div);
 });
