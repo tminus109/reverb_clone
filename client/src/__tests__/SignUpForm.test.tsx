7c106f4f1a3c67ac0c9050305fcaad4c22292c72
tminus109 <martontibor123@gmail.com>
2021-12-13 22:12:54 +0100

    cleanup: SignUpForm.test.tsx

--- client/src/__tests__/SignUpForm.test.tsx
+++ client/src/__tests__/SignUpForm.test.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SignUpForm from "../components/forms/SignUpForm";
 
